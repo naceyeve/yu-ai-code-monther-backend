02d996b5200e6b915cc479f53f900e043aa2f660
penngli <271948021@qq.com>
2025-08-04 00:20:16 +0800

    1，新建项目

---
+++ src/main/java/com/pengli/yuaicodemontherbackend/YuAiCodeMontherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pengli.yuaicodemontherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuAiCodeMontherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAiCodeMontherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pengli/yuaicodemontherbackend/YuAiCodeMontherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pengli.yuaicodemontherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuAiCodeMontherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
